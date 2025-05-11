5df7cac8262c0317ea9a939845b82d6d5f4d663d
Mohammad m <mashhadim901@gmail.com>
2025-05-11 13:08:35 +0430

    feat:login ui added

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
 import './style.css'
+import { loginPage } from './pages/login-page';
 
+import Navigo from 'navigo';
+import { emailIcon } from './utils/icons';
+const app = document.getElementById('app')
+
+const router = new Navigo('/');
+router.on('/login', () => {
+//   alert("/about")
+
+loginPage()
+
+}).on('/signup',()=>{
+    console.log("tesyyyyyy");
+    
+})
+router.resolve();
